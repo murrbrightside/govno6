12e55c941ecc57facb859956d8c99d7770caf6d4
Усажа <murr.brightside@yandex.ru>
2023-03-26 00:27:29 +0300

    хоть бы завтра меня переехала электричка и мне не пришлось бы ждать когда дмитрий бизин пришлет мне очередное указание на недочеты через десять лет

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 public class Main {
     public static void leapYear(int y) {
-        if(y%4 == 0 || y %100 ==0){
+        if( y %100 ==0 && y% 400 ==0){
             System.out.println("Високосный год");
-        }else {
+
+        } else if (y % 4 == 0 && y %100 != 0) {
+            System.out.println("Високосный год");
+        } else {
             System.out.println("Невисокосный год");
 
 
         }}
-    public static void version(int x){
+    public static void version(int x, int w){
         int currentYear = 2023;
+        if(w == 0){
         if (x < currentYear){
-            System.out.print("Установите облегченную версию");
+            System.out.println("Установите облегченную версию для Айфона");
         }else{
-            System.out.print("Установите текущую версию");}
-    }
+            System.out.println("Установите текущую версию для Айфона");}}
+        else{if (x < currentYear){
+            System.out.println("Установите облегченную версию для Андроид");
+        }else{
+            System.out.println("Установите текущую версию для Андроид");}}
+
+        }
+
     public static  int  delivery(int distance){
         if (distance < 60){
             if (distance < 20){return 1;
@@ -36,20 +46,14 @@ public class Main {
     }
     public static void task1(){
         ///Задача 1
-        int year = 2023;
+        int year = 1900;
         leapYear(year);
     }
     public static void task2(){
 
         int yearOfPhone = 2023;
         int versionOfPhone = 1;
-        if (versionOfPhone == 1){
-            version(yearOfPhone);
-            System.out.println(" для Андроида");
-
-        }else{version(yearOfPhone);
-            System.out.println(" для Айфона");}
-
+        version(yearOfPhone, versionOfPhone);
 
     }
     public static void task3(){
